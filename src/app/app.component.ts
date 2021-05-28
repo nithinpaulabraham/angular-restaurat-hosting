30b4d372cd3ecc8473fbc5744366cfa32abfb7ac
Nithin Paul Abraham <nithinabraham91@gmail.com>
2021-05-28 14:27:45 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  image1: string ="assets/images/salad.jpg";
+  image2: string ="assets/images/chicken-steak.jpg";
+  image3: string ="assets/images/pasta.jpg";
+  image4: string ="assets/images/curry.jpg";
+  image5: string ="assets/images/team-1.jpg";
+  image6: string ="assets/images/team-2.jpg";
+  image7: string ="assets/images/team-3.jpg";
+  image8: string ="assets/images/team-4.jpg";
+}
